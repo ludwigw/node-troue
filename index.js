9dc8086e7e432aa31dbf337430940cb98a4f4951
Ludwig Wendzich <ludwig@wendzich.com>
2017-06-22 22:06:19 +1200

    Move everything to Campaign Monitor

--- index.js
+++ index.js
@@ -17,6 +17,38 @@ var async = require('async');
 
 var app = express();
 
+var createRSVP = function (result) {
+
+	var response = {
+		Name: result.Name,
+		Email: result.EmailAddress,
+	};
+
+	// Flatten Custom Fields
+	result.CustomFields.forEach(function(field) {
+		response[field.Key] = field.Value;
+	});
+
+	// Update RSVP Value
+	switch(parseInt(response.RSVP)) {
+		case 0:
+		response.hasRSVP = false;
+		break;
+
+		case 1:
+		response.hasRSVP = true;
+		response.RSVP = false;
+		break;
+
+		case 2:
+		response.RSVP = true;
+		response.hasRSVP = true;
+		break;
+	}
+
+	return response;
+};
+
 app.engine('handlebars', exphbs({
 	defaultLayout: 'main',
 	helpers: {
@@ -49,35 +81,29 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.get('/', (request, response) => {
 
 	var q = request.query;
+	var rsvp;
 
 	if(q.token && q.email) {
-		mailchimp.get('/lists/' + list + '/members/' + md5(q.email.toLowerCase())).then( result => {
-			if (result.unique_email_id != q.token) return null;
-			return result;
-		}).then( result => {
-			var hasRSVP, RSVP;
 
-			switch(result.merge_fields.RSVP) {
-				case 0:
-				hasRSVP = false;
-				break;
+		createsend.subscribers.getSubscriberDetails(cmList, q.email, (err, res) => {
 
-				case 1:
-				hasRSVP = true;
-				RSVP = false;
-				break;
+			var rsvp;
 
-				case 2:
-				RSVP = true;
-				hasRSVP = true;
-				break;
+			if(err) {
+				console.log('CM Error:', err);
+			} else {
+
+				rsvp = createRSVP(res);
+
+				if (rsvp.Token != q.token) {
+					rsvp = null;
+				}	
 			}
 
 			response.render('home', {
-				result,
-				RSVP,
-				hasRSVP
+				rsvp
 			});
+
 		});
 	} else {
 		response.render('home');
@@ -87,7 +113,8 @@ app.get('/', (request, response) => {
 app.post('/rsvp', (request, response) => {
 	var p = request.body;
 
-	if((p.token && p.answer)) {
+	if((p.email && p.token && p.answer)) {
+		var data = [];
 		var RSVP;
 		var DIETARY;
 
@@ -108,30 +135,41 @@ app.post('/rsvp', (request, response) => {
 
 		DIETARY = p['dietary-requirements'] ? p['dietary-requirements'] : null;
 
-		mailchimp.patch('/lists/' + list + '/members/' + p.token, {
-			'merge_fields' :{
-				RSVP,
-				DIETARY
+		if(DIETARY) data.push({Key: 'Dietary', Value: DIETARY});
+		if(RSVP) data.push({Key: 'RSVP', Value: RSVP});
+
+		async.waterfall([
+			(done) => {
+				createsend.subscribers.updateSubscriber(cmList, p.email, {
+					CustomFields: data
+				}, done);
+			},
+			(done) => {
+				setTimeout(()=> {
+					createsend.subscribers.getSubscriberDetails(cmList, p.email, done);
+				},100);
 			}
-		}).then( result => {
-			var RSVP;
+		], (err, res) => {
+			var rsvp;
 
-			switch(result.merge_fields.RSVP) {
-				case 1:
-				RSVP = null;
-				break;
+			if(err) {
+				console.log('CM Error:', err);
+				response.status(500);
+				response.send();
+			} else {
 
-				case 2:
-				RSVP = true;
-				break;
+				rsvp = createRSVP(res);
+
+				// API is async so when I read, I can’t guarantee the write has finished?
+				// So make sure it says the right thing.
+				rsvp.RSVP = (RSVP > 1 ? true : false);
+
+				response.render('rsvp', {
+					rsvp
+				});
 			}
-
-			response.render('rsvp', {
-				result,
-				RSVP,
-				DIETARY
-			});
 		});
+
 	} else {
 		response.redirect('/');
 	}
@@ -139,33 +177,53 @@ app.post('/rsvp', (request, response) => {
 
 app.post('/login', (request, response) => {
 	var p = request.body;
+	var rsvp;
+
+	// Create a details object
+	var details = {
+		smartEmailID: '43f81b90-4160-4226-bdeb-e815c408f8f5'
+	};
 
 	if(p.email) {
-		mailchimp.get('/lists/' + list + '/members/' + md5(p.email)).then( result => {
 
-			mandrill.messages.send({
-				"message": require("./login.js")(result),
-				"async": false,
-				"ip_pool": "Main Pool",
-				"send_at": "2000-01-05 12:42:01"
-			}, function(email_result) {
-    			response.render('login', {
-					result,
-					email_result
+		async.waterfall([
+			(done) => {
+
+				createsend.subscribers.getSubscriberDetails(cmList, p.email, (err, res) => {
+
+					if(err) {
+						done("No invite");
+					} else {
+
+						rsvp = createRSVP(res);
+
+						details.to = rsvp.Name + " <" + rsvp.Email + ">";
+						details.data = {
+							"Token": rsvp.Token,
+							"email": rsvp.Email
+						}
+
+						done();
+					}
+				});
+			},
+			(done) => {
+
+				createsend.transactional.sendSmartEmail(details, function (err, res) {
+				    if (err) {
+				        done(err);
+				    } else {
+				        done();
+				    }
 				});
 
-			}, function(e) {
-			    // Mandrill returns the error as an object with name and message keys
-			    console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
-				response.redirect('/');
-
-			});
-
-		}, result => {
+			}
+		], (err, res) => {
 			response.render('login', {
-					
-				});
+				rsvp
+			});
 		});
+
 	} else {
 		response.redirect('/');
 	}
@@ -174,29 +232,29 @@ app.post('/login', (request, response) => {
 app.get('/list', (request, response) => {
 	var token = request.query ? request.query['token'] : null;
 
-	if(token && token == process.env.MAILCHIMP_KEY) {
-		mailchimp.get('/lists/' + list + '/members/?count=100&status=subscribed').then( result => {
+	if(token && token == process.env.CREATESEND_KEY) {
+
+		createsend.lists.getActiveSubscribers(cmList, '', (res) => {
+
 			var COMING = 0, NOT_COMING = 0, WAITING = 0;
 			var coming = [], not_coming = [], waiting = [];
 
-			var members = result.members.sort(function (a, b) {
-			  return b.merge_fields.RSVP - a.merge_fields.RSVP;
-			});
+			res.Results.forEach(function(result){
+				var member = createRSVP(result);
 
-			members.forEach(function(member){
-				switch(member.merge_fields.RSVP){
+				switch(member.RSVP){
 					case 2:
-					COMING += member.merge_fields.COUNT;
+					COMING += member.Count;
 					coming.push(member);
 					break;
 
 					case  1:
-					NOT_COMING += member.merge_fields.COUNT;
+					NOT_COMING += member.Count;
 					not_coming.push(member);
 					break;
 
 					default:
-					WAITING += member.merge_fields.COUNT;
+					WAITING += member.Count;
 					waiting.push(member);
 					break;
 				}
@@ -211,68 +269,77 @@ app.get('/list', (request, response) => {
 				WAITING
 			});
 		});
+
 	} else {
 		response.redirect('/');
 	}
 
 });
 
+// Let’s MailChimp confirm the webhook handler exists
 app.get('/notify', (request, response) => {
 	response.status(200);
     response.send();
 
 });
 
+// Send a notification to Natalie and myself that an RSVP has occurred.
 app.post('/notify', (request, response) => {
-	var p = request.body.data;
 
-	var NAME = p.merges.NAME;
-	var EMAIL = p.email;
-	var COUNT = p.merges.COUNT;
-	var DIETARY = p.merges.DIETARY || '';
-	var RSVP = parseInt(p.merges.RSVP) > 1 ? "Yes" : "No";
+	var rsvp = createRSVP(request.body.Events[0]);
+	var details = {
+		smartEmailID: '4829db53-0d8d-4289-b91c-90df6f76443c',
+		to: [
+			"Ludwig Wendzich <ludwig@wendzich.com>",
+			"Natalie Theron <theron.natalie@gmail.com"
+		],
+		data: {
+		    "Name": rsvp.Name,
+			"RSVP": rsvp.RSVP,
+			"Count": rsvp.Count,
+			"Dietary": rsvp.Dietary || '',
+			"Token": rsvp.Token,
+			"email": rsvp.Email
+		}
+	}
 
-	mandrill.messages.send({
-		"message": {
-	        "html": "<p>" + NAME + "(" + COUNT + ") has RSVPd (" + RSVP + ").</p><p>" + DIETARY + "</p>",
-	        "subject": "RSVP: " + NAME + " (" + RSVP + ")",
-	        "from_email": EMAIL,
-	        "from_name": NAME,
-	        "auto_text": true,
-	        "to": [{
-	                "email": "ludwig@wendzich.com",
-	                "name": "Ludwig Wendzich",
-	                "type": "to"
-	            },
-	            {
-	                "email": "theron.natalie@gmail.com",
-	                "name": "Natalie Theron",
-	                "type": "to"
-	            }],
-	        "headers": {
-	            "Reply-To": "ludwig@wendzich.com"
-	        },
-	        "view_content_link": true
-	    },
-		"async": false,
-		"ip_pool": "Main Pool",
-		"send_at": "2000-01-05 12:42:01"
-	}, function(result) {
+	createsend.transactional.sendSmartEmail(details, function (err, res) {
+		if (err) {
+		    console.log('A CM error occurred: ' + e.name + ' - ' + e.message);
+			response.redirect('/');
+		} else {
 			response.status(200);
 		    response.send();
-
-	}, function(e) {
-	    // Mandrill returns the error as an object with name and message keys
-	    console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
-		response.redirect('/');
-
+		}
 	});
-
-	response.status(200);
-    response.send();
 
 });
 
+app.get('/create-webhook', (request, response) => {
+
+	createsend.lists.createWebhook(cmList, {
+	    "Events": [ "Update" ],
+	    "Url": "ludnat.wendzich.com/notify",
+	    "PayloadFormat": "json"
+	}, (err, res) => {
+		console.log(res);
+		response.status(200);
+		response.send();
+	});
+
+});
+
+app.get('/test-webhook', (request, response) => {
+
+	createsend.lists.testWebhook(cmList, 'a4e05a7a89bd9d670062efd89faa6523', (err, res) => {
+		console.log(err, res);
+		response.status(200);
+		response.send();
+	});
+
+});
+
+// Save MailChimp Unique Email ID to Merge Field: TOKEN
 app.get('/tokenize', (request, response) => {
 
 	mailchimp.get('/lists/' + list + '/members/?count=100&status=subscribed').then( result => {
@@ -307,6 +374,8 @@ app.get('/tokenize', (request, response) => {
 		});
 });
 
+// Copy details from MailChimp to CampaignMonitor
+// Requires Members in MailChimp to already exist as Subscribers in CampaignMonitor
 app.get('/transfer', (request, response) => {
 
 	mailchimp.get('/lists/' + list + '/members/?count=100&status=subscribed').then( result => {
@@ -315,7 +384,7 @@ app.get('/transfer', (request, response) => {
 			var calls = [];
 
 			members.forEach(function(member){
-				calls.push((done)=> {
+				calls.push((done) => {
 
 					createsend.subscribers.updateSubscriber(cmList, member.email_address, {
 						Name: member.merge_fields.NAME,
